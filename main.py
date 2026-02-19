c2fb57a8dc10bfd2bd9ecdefa1da157048d25c19
DJY <1055411737@qq.com>
2026-02-19 20:53:59 +0800

    更新

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.api import AstrBotConfig, logger
 from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
     AiocqhttpMessageEvent,
 )
@@ -8,8 +8,13 @@ from astrbot.core.star.filter.platform_adapter_type import PlatformAdapterType
 
 
 class JoinLog(Star):
-    def __init__(self, context: Context):
-        super().__init__(context)
+    def __init__(self, context: Context, config: AstrBotConfig):
+        super().__init__(context, config)
+        self.log: bool = bool(config.get("log", True))
+
+    async def log(self, string: str):
+        if self.log:
+            logger.info(string)
 
     @filter.platform_adapter_type(PlatformAdapterType.AIOCQHTTP)
     async def handle_group_add(self, event: AiocqhttpMessageEvent):
@@ -21,7 +26,7 @@ class JoinLog(Star):
         user_id = str(raw.get("user_id"))
         group_id = str(raw.get("group_id"))
 
-        # logger.info(f"测试: {raw}")
+        # await self.log(f"测试: {raw}")
 
         if post_type == "request" and raw.get("request_type") == "group":
             sub_type = raw.get("sub_type")
@@ -42,11 +47,11 @@ class JoinLog(Star):
 
             if sub_type == "add":
                 # 加群
-                logger.info(f"收到用户 {user_id} 的加群申请")
-                logger.info(f"问题: {question}")
-                logger.info(f"回答: {answer}")
-                logger.info(f"验证方式: {verify_type}")
-                logger.info(f"请求标识: {flag}")
+                await self.log(f"收到用户 {user_id} 的加群申请\
+                               问题: {question}\
+                               回答: {answer}\
+                               验证方式: {verify_type}\
+                               请求标识: {flag}")
 
         elif post_type == "notice":
             notice_type = raw.get("notice_type")
@@ -56,14 +61,15 @@ class JoinLog(Star):
                 sub_type = raw.get("sub_type")
                 operator_id = raw.get("operator_id")
                 if sub_type == "approve":
-                    logger.info(f"用户 {user_id} 被管理员 {operator_id} 同意加入群 {group_id}")
+                    await self.log(f"用户 {user_id} 被管理员 {operator_id} 同意加入群 {group_id}")
+                    
                 elif sub_type == "invite":
-                    logger.info(f"用户 {user_id} 被 {operator_id} 邀请加入群 {group_id}")
+                    await self.log(f"用户 {user_id} 被 {operator_id} 邀请加入群 {group_id}")
             # 减少
             elif notice_type == "group_decrease":
                 sub_type = raw.get("sub_type")
                 operator_id = raw.get("operator_id")
                 if sub_type == "leave":
-                    logger.info(f"用户 {user_id} 主动退出群 {group_id}")
+                    await self.log(f"用户 {user_id} 主动退出群 {group_id}")
                 elif sub_type == "kick":
-                    logger.info(f"用户 {user_id} 被管理员 {operator_id} 踢出群 {group_id}")
+                    await self.log(f"用户 {user_id} 被管理员 {operator_id} 踢出群 {group_id}")
